5911c43b62d73db67919cbaccca0bb03ac5c0d68
guahmed <guetat.ahmed24@gmail.com>
2020-07-08 12:12:22 +0100

    4 commit

--- src/Controller/ParentController.php
+++ src/Controller/ParentController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Children;
+use App\Entity\CompteClub;
+use App\Entity\ParentChild;
 use App\Repository\ClubRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,39 +24,89 @@ class ParentController extends AbstractController
         ]);
     }
 
+    
+
 
     /**
-     * @Route("/parentspace", name="parentespace")
+     * @Route("/spaceparent", name="spaceparent")
      */
+
     public function ParentMainPage(ClubRepository $repoClub)
     {
-       $clubs = $repoClub->findAll();
-       dump($clubs);
-        return $this->render('parent/mainPageParent.html.twig',
-            ['clubs'=>$clubs]);
+        $clubs = $repoClub->findAll();
+        return $this->render(
+            'parent/mainPageParent.html.twig',
+            ['clubs' => $clubs]
+        );
+    }
+
+
+    /**
+     * @Route("/parentspace/{id}/inscrir", name="club_inscription")
+     */
+    public function club_inscription(
+        Request $request,
+        EntityManagerInterface $manager,
+        ClubRepository $repoClub,
+        $id
+    ) {
+
+        if ($request->request->count() > 2) {
+            $club = $repoClub->findOneById($id);
+            dump($club);
+            $child = new Children();
+
+            $child->setName($request->request->get('Nom'));
+            $child->setLastName($request->request->get('Prenom'));
+            $child->setAge($request->request->get('Age'));
+            $child->setTelphone($request->request->get('Telephone'));
+            $child->setAdress($request->request->get('Adresse'));
+            $child->setGovermnet($request->request->get('Gouvernerat'));
+            $child->SetPostalCode($request->request->get('CodePostal'));
+            $child->setRole("children");
+
+            $compteClub = new CompteClub();
+            $compteClub->setMonthlyBill($request->request->get("Payemenbt"));
+            $compteClub->setEnfant($child);
+            $compteClub->setClub($club);
+
+            $manager->persist($child);
+            $manager->persist($compteClub);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('spaceparent');
+        }
+
+
+        return $this->render('parent/childInscription.html.twig');
     }
 
     /**
-    * @Route("/parentspace/{id}/inscrir", name="club_inscription")
-    */
-    public function club_inscription(Request $request,EntityManagerInterface $manager)
+     * @Route("/inscriptionparent", name="parentespace")
+     */
+    public function Inscriptionparent( Request $request, EntityManagerInterface $manager)
     {
-    
-    
-     return $this->render ('parent/childInscription.html.twig') ;
+        if ($request->request->count() > 2) {
+           
+            $parent = new ParentChild();
+
+            $parent->setName($request->request->get('Nom'));
+            $parent->setLastName($request->request->get('Prenom'));
+            $parent->setAge($request->request->get('Age'));
+            $parent->setTelphone($request->request->get('Telephone'));
+            $parent->setAdress($request->request->get('Adresse'));
+            $parent->setGovermnet($request->request->get('Gouvernerat'));
+            $parent->SetPostalCode($request->request->get('CodePostal'));
+            $parent->setRole("parent");
+
+            $manager->persist($parent);
+            $manager->flush();
+
+            return $this->redirectToRoute('spaceparent');
+
+        }
+
+        return $this->render('parent/InscriptionParent.html.twig');
     }
-
 }
-/*
-$childss=new Children();
-$form = $this->createFormBuilder($childss);
-$form->add('name',TextType::class);
-$form->add('lastName');
-$form->add('age');
-$form->add('telphone');
-$form->add('adress');
-$form->add('postalCode');
-$form->getForm();
-
-$form->handleRequest($request);
-*/
